fd36704d14e1804629f8b12c7c7bf39e83f7b291
Victor Pena <vicpenap@gmail.com>
2012-04-12 10:11:15 +0200

    Improved model

--- VPPCoreDataExample/Model/Quote.h
+++ VPPCoreDataExample/Model/Quote.h
@@ -2,17 +2,19 @@
 //  Quote.h
 //  VPPCoreDataExample
 //
-//  Created by Víctor on 14/02/12.
+//  Created by Víctor on 12/04/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
+@class Author;
 
 @interface Quote : NSManagedObject
 
 @property (nonatomic, retain) NSDate * date;
 @property (nonatomic, retain) NSString * quote;
+@property (nonatomic, retain) Author *author;
 
 @end
